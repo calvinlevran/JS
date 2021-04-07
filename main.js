a10b7eaf1a8f6b89db0b809a91f68ce35a235710
calvinlevran <calvin021@binus.ac.id>
2021-04-07 10:55:09 +0700

    push let

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-// ES5
-var myName = "Calvin"
-myName = "mik"
-console.log(myName)
+// // ES5
+// var myName = "Calvin"
+// myName = "mik"
+// console.log(myName)
 
-// ES6
-//const / constant
-const myCar = "Mazda EX-7"
-// const myCar = "Honda Freed"
-// myCar = "Honda ABC"
-console.log(myCar);
+// // ES6
+// //const / constant
+// const myCar = "Mazda EX-7"
+// // const myCar = "Honda Freed"
+// // myCar = "Honda ABC"
+// console.log(myCar);
+
+var myCarsVar = ["BMW", "Tesla", "Toyota"];
+
+for(var i = 0; i<myCarsVar.length; i++){
+    console.log("My Cars Vars = " + myCarsVar[i])
+}
+
+console.log(i);
+
+const myCarsLet = ["BMW", "Tesla", "Toyota"];
+
+for(let x = 0; x<myCarsVar.length; x++){
+    console.log("My Cars Vars = " + myCarsLet[x])
+}
+
+console.log(x);
