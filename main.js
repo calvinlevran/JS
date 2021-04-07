61af20265fe1c3225cf5bcc53a17e8512eda389d
calvinlevran <calvin021@binus.ac.id>
2021-04-07 10:27:26 +0700

    init project

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello world!')
+// ES5
+var myName = "Calvin"
+myName = "mik"
+console.log(myName)
+
+// ES6
+//const / constant
+const myCar = "Mazda EX-7"
+// const myCar = "Honda Freed"
+// myCar = "Honda ABC"
+console.log(myCar);
