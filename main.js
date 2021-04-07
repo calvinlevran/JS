731f0feba6c305388bb18d537a9c54082ec1a6cc
calvinlevran <calvin021@binus.ac.id>
2021-04-07 11:53:27 +0700

    belajar array forEach

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-// ES5
-var myName = "Calvin"
-myName = "mik"
-console.log(myName)
+// Ambil Element
+const skills = document.getElementById("skills");
 
-// ES6
-//const / constant
-const myCar = "Mazda EX-7"
-// const myCar = "Honda Freed"
-// myCar = "Honda ABC"
-console.log(myCar);
+//buat variabel
+const mySkills = ["ES6", "UX Design", "Git"];
+
+mySkills.push("Dev Ops");
+
+console.log(mySkills);
+
+//normal function
+function showSkill(mySkill){
+    console.log(mySkill);
+}
+
+//arrow funct
+const showSkill2 = (mySkill) => { 
+    console.log(mySkill);
+}
+
+// forEach
+mySkills.forEach(function (mySkill){
+    console.log(mySkill);
+});
+
+let parent = '<ul>';
+
+mySkills.forEach((mySkill) => {
+    parent += `<li>${mySkill}</li>`
+});
+
+parent += '</ul>';
+
+mySkills.forEach(showSkill2);
+skills.innerHTML = parent;
