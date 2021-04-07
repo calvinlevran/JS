2ad9e641e9c6d7cc5ec5565d1cf7c90519ab341a
calvinlevran <calvin021@binus.ac.id>
2021-04-07 13:45:36 +0700

    belajar fetch data dari API

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-// ES5
-var myName = "Calvin"
-myName = "mik"
-console.log(myName)
+const product = document.getElementById("products");
+const allProducts = 'https://fakestoreapi.com/products'
 
-// ES6
-//const / constant
-const myCar = "Mazda EX-7"
-// const myCar = "Honda Freed"
-// myCar = "Honda ABC"
-console.log(myCar);
+function createNode(element){
+    return document.createElement(element);
+}
+
+function append(parent, el){
+    return parent.appendChild(el);
+}
+
+fetch(allProducts)
+    .then((response) => response.json())
+    .then ((json) => {
+        console.log(json)
+        json.map((product) => {
+            let li = createNode("li");
+            let img = createNode("img");
+            let span = createNode("span");
+
+            img.setAttribute("width","200")
+
+            img.src = product.image;
+            span.innerHTML = `${product.title}, ${product.price}`
+
+            append(li, img);
+            append(li, span);
+            append(products, li);
+        })
+    })
+    .catch((error) => { console.log(error)});
