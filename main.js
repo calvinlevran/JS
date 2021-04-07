26dd411306feaca52f69db1ba6a1ece542fd39a2
calvinlevran <calvin021@binus.ac.id>
2021-04-07 11:21:04 +0700

    belajar object literal

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-// ES5
-var myName = "Calvin"
-myName = "mik"
-console.log(myName)
+// OBJECT LITERALS
+const cart = document.getElementById("cart");
 
-// ES6
-//const / constant
-const myCar = "Mazda EX-7"
-// const myCar = "Honda Freed"
-// myCar = "Honda ABC"
-console.log(myCar);
+//ES5
+// function addProduct(name,category){
+//     return{
+//         name: name,
+//         category: category
+//     }
+// }
+
+// var getProduct = addProduct("iPhone 12", "Smartphone");
+
+// console.log(getProduct);
+
+// cart.innerHTML = `product 1: ${getProduct.name}, ${getProduct.category}`;
+
+//ES6
+function addProduct(name,category){
+    return{productName: name, category}
+}
+
+var getProduct = addProduct("iPhone 12", "Smartphone");
+
+console.log(getProduct);
+
+cart.innerHTML = `product 1: ${getProduct.productName}, ${getProduct.category}`;
